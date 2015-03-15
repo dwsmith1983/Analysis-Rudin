e3f7a01476cc56528b1ab50823e9f4c2bee0ebec
Dustin <dustin.smith@uvm.edu>
2015-03-14 20:44:12 -0400

    update

--- auto/chapter4.el
+++ auto/chapter4.el
@@ -2,5 +2,9 @@
  "chapter4"
  (lambda ()
    (LaTeX-add-labels
-    "ch4")))
+    "ch4"
+    "4.4"
+    "4.5"
+    "4.9"
+    "4.13")))
 
--- auto/rudin.el
+++ auto/rudin.el
@@ -3,14 +3,14 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenx" "utf8") ("mathpazo" "sc" "osf") ("eulervm" "euler-digits" "small") ("fontenc" "T1") ("geometry" "margin=0.5in") ("microtype" "stretch=10" "verbose=silent" "protrusion=0") ("hyperref" "pdfencoding=auto" "psdextra" "bookmarksdepth=4") ("cleveref" "noabbrev") ("hypcap" "all")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "fixltx2e"
     "inputenx"
@@ -32,5 +32,6 @@
     "hypcap")
    (TeX-add-symbols
     "myshade"
-    "eqnumtag")))
+    "eqnumtag"
+    "diam")))
 
